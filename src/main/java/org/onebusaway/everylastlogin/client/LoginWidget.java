9670931f7932e480f09d3b66299b766e67d73b44
Brian Ferris <bdferris@google.com>
2010-06-11 03:26:57 +0000

    Proper escaping of the target url in the LoginWidget

--- src/main/java/org/onebusaway/everylastlogin/client/LoginWidget.java
+++ src/main/java/org/onebusaway/everylastlogin/client/LoginWidget.java
@@ -5,6 +5,7 @@ import com.google.gwt.dom.client.AnchorElement;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.logical.shared.CloseEvent;
 import com.google.gwt.event.logical.shared.CloseHandler;
+import com.google.gwt.http.client.URL;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
@@ -49,7 +50,7 @@ public class LoginWidget extends Composite {
     initWidget(_uiBinder.createAndBindUi(this));
 
     _baseUrl = baseUrl;
-    _target = target;
+    _target = URL.encodeComponent(target);
 
     setOpenIdHref(_googleLink, "https://www.google.com/accounts/o8/id");
     setOpenIdHref(_yahooLink, "http://yahoo.com/");
